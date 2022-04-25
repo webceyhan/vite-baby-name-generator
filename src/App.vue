8d31551fb9387a9fdeab05f98c8618fb7fa5f2a3
Evren Ceyhan <evren@ceyhan.io>
2022-04-25 13:32:03 +0300

    basic template impl.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="container text-center p-5">
-    <h1 class="display-4">
-      home page
-    </h1>
+    <h1 class="display-5 mb-5">Baby Name Generator</h1>
+
+    <div class="card">
+      <div class="card-body">
+        <h4>1) Choose a gender</h4>
+        boy girl unisex
+      </div>
+
+      <div class="card-body">
+        <h4>2) Choose the name's popularity</h4>
+        trendy unique
+      </div>
+
+      <div class="card-body">
+        <h4>3) Choose name's length</h4>
+        short long all
+      </div>
+    </div>
   </div>
 </template>
