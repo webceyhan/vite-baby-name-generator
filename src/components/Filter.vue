d491caec797f5e78f777375dfde0de364fd530b6
Evren Ceyhan <evren@ceyhan.io>
2022-04-25 14:30:46 +0300

    filter buttons set to large

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
-
 const emit = defineEmits(["update:modelValue"]);
 
 defineProps<{
-    name: string,
-    options: string[];
-    modelValue: any;
+  name: string;
+  options: string[];
+  modelValue: any;
 }>();
 </script>
 
 <template>
-    <div class="btn-group">
-        <template v-for="(opt, i) in options" :key="i">
-            <input type="radio" class="btn-check" :name="name" :id="`${name}-${i}`" autocomplete="off"
-                @change="emit('update:modelValue', opt)" :checked="modelValue === opt" :value="opt" />
-            <label class="btn btn-outline-primary text-capitalize" :for="`${name}-${i}`">{{ opt }}</label>
-        </template>
-    </div>
+  <div class="btn-group btn-group-lg">
+    <template v-for="(opt, i) in options" :key="i">
+      <input
+        type="radio"
+        class="btn-check"
+        :name="name"
+        :id="`${name}-${i}`"
+        autocomplete="off"
+        @change="emit('update:modelValue', opt)"
+        :checked="modelValue === opt"
+        :value="opt"
+      />
+      <label class="btn btn-outline-primary text-capitalize px-5" :for="`${name}-${i}`">{{
+        opt
+      }}</label>
+    </template>
+  </div>
 </template>
