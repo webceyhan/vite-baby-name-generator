e1d4063fa23309a67fca60fddaf5de7c4f3303fa
Evren Ceyhan <evren@ceyhan.io>
2022-04-25 13:39:51 +0300

    filter radio buttons added

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,48 @@
     <div class="card">
       <div class="card-body">
         <h4>1) Choose a gender</h4>
-        boy girl unisex
+
+        <div class="btn-group">
+          <input type="radio" class="btn-check" name="btnradio-gender" id="btnradio-gender1" autocomplete="off"
+            checked />
+          <label class="btn btn-outline-primary" for="btnradio-gender1">Boy</label>
+
+          <input type="radio" class="btn-check" name="btnradio-gender" id="btnradio-gender2" autocomplete="off" />
+          <label class="btn btn-outline-primary" for="btnradio-gender2">Girl</label>
+
+          <input type="radio" class="btn-check" name="btnradio-gender" id="btnradio-gender3" autocomplete="off" />
+          <label class="btn btn-outline-primary" for="btnradio-gender3">Unisex</label>
+        </div>
       </div>
 
       <div class="card-body">
         <h4>2) Choose the name's popularity</h4>
-        trendy unique
+
+        <div class="btn-group">
+          <input type="radio" class="btn-check" name="btnradio-popularity" id="btnradio-popularity1" autocomplete="off"
+            checked />
+          <label class="btn btn-outline-primary" for="btnradio-popularity1">Trendy</label>
+
+          <input type="radio" class="btn-check" name="btnradio-popularity" id="btnradio-popularity2"
+            autocomplete="off" />
+          <label class="btn btn-outline-primary" for="btnradio-popularity2">Unique</label>
+        </div>
       </div>
 
       <div class="card-body">
         <h4>3) Choose name's length</h4>
-        short long all
+
+        <div class="btn-group">
+          <input type="radio" class="btn-check" name="btnradio-length" id="btnradio-length1" autocomplete="off"
+            checked />
+          <label class="btn btn-outline-primary" for="btnradio-length1">Short</label>
+
+          <input type="radio" class="btn-check" name="btnradio-length" id="btnradio-length2" autocomplete="off" />
+          <label class="btn btn-outline-primary" for="btnradio-length2">Long</label>
+
+          <input type="radio" class="btn-check" name="btnradio-length" id="btnradio-length3" autocomplete="off" />
+          <label class="btn btn-outline-primary" for="btnradio-length3">All</label>
+        </div>
       </div>
     </div>
   </div>
