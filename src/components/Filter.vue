b47113541a320b961b2d05ddf60f8caf66b83cdd
Evren Ceyhan <evren@ceyhan.io>
2022-04-25 14:28:57 +0300

    bugfix: @checked must be :checked

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -13,7 +13,7 @@ defineProps<{
     <div class="btn-group">
         <template v-for="(opt, i) in options" :key="i">
             <input type="radio" class="btn-check" :name="name" :id="`${name}-${i}`" autocomplete="off"
-                @change="emit('update:modelValue', opt)" @checked="modelValue === opt" :value="opt" />
+                @change="emit('update:modelValue', opt)" :checked="modelValue === opt" :value="opt" />
             <label class="btn btn-outline-primary text-capitalize" :for="`${name}-${i}`">{{ opt }}</label>
         </template>
     </div>
